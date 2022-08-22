28a0199843fb64d985fbc5cfb4f94d9b4dd3484e
user <user@DESKTOP-CJC98TJ>
2022-08-22 17:16:41 -0300

    Branch hotfix

--- GCMS Pratica Git/src/HelloWorld.java
+++ GCMS Pratica Git/src/HelloWorld.java
@@ -1,8 +1,8 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
-        System.out.println("Hello, Earth!"); 
-        System.out.println("Hello, Moon!");
-        System.out.println("Hello, Mars");
+        System.out.println("Hello, Mother!"); 
+        System.out.println("Hello, Father!");
+        System.out.println("Hello, Brother");
     }
 }
