a9e88c8f72e636d076a4e7c0afdaa6ba1e831f6f
user <user@DESKTOP-CJC98TJ>
2022-08-22 17:19:07 -0300

    No !

--- GCMS Pratica Git/src/HelloWorld.java
+++ GCMS Pratica Git/src/HelloWorld.java
@@ -1,8 +1,8 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
-        System.out.println("Hello, Earth!"); 
-        System.out.println("Hello, Moon!");
+        System.out.println("Hello, Earth"); 
+        System.out.println("Hello, Moon");
         System.out.println("Hello, Mars");
         System.out.println("Hello, Saturn");
     }
